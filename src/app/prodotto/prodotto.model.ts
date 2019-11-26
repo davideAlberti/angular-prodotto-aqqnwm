7f73d4f814fa0216d611845fe2607527bba23071
davideAlberti <davide.alberti@marconirovereto.it>
2019-11-26 13:27:36 +0100

    2 files modified

--- src/app/prodotto/prodotto.model.ts
+++ src/app/prodotto/prodotto.model.ts
@@ -1,46 +1,74 @@
-import { Router } from '@angular/router'
+const MINSTOCK: number = 10;
 
 export class Prodotto {
-  codice: string
-  nome: string
-  prezzo: number
-  dataScadenza: Date
-  quantita: number
+  codice: string;
+  nome: string;
+  prezzo: number;
+  dataScadenza: Date;
+  quantita: number;
 
   constructor(codice: string, nome: string, prezzo: number, dataScadenza: Date, quantita: number) {
-    this.codice = codice
-    this.nome = nome
-    this.prezzo = prezzo
-    this.dataScadenza = dataScadenza
-    this.quantita = quantita
+    this.codice = codice;
+    this.nome = nome;
+    this.prezzo = prezzo;
+    this.dataScadenza = dataScadenza;
+    this.quantita = quantita;
   }
 
-  get isScaduto(): boolean {
-    return this.dataScadenza < new Date()
-  }
-
-  get getColor(): string {
-    if(this.isScaduto) return 'red'
-    else return 'green'
-  }
-
-  get getGiorniScadenza() {
-    return Math.abs(this.diff(new Date(), this.dataScadenza))
-  }
-
-  private diff(date1, date2) {
-    let dt1 = new Date(date1);
-    let dt2 = new Date(date2);
-    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) / (1000 * 60 * 60 * 24));
-  }
-
-  get formatDate() {
+  get DataScadenza() {
     return this.dataScadenza.toISOString().substring(0, 10);
   }
 
-  set formatDate(text: any){
-    let elem = text.split('-');
-    let d = new Date(Date.UTC(elem[0], elem[1]-1, elem[2]));
-    this.dataScadenza.setFullYear(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate());
+  set DataScadenza(data) {
+    this.dataScadenza = new Date(data);   
+  }
+
+  isScadutoOInScadenza(): boolean {
+    return this.giorniAScadenza() < 7;
+  }
+
+  getScadenzaColor(): string {
+    if (this.giorniAScadenza() <= 0) {
+      return 'red';
+    }
+    if (this.giorniAScadenza() < 7) {
+      return 'orange';
+    }
+    return 'green';
+  }
+
+  getScadenzaLabel(): string {
+    if (this.giorniAScadenza() < 0) {
+      return 'Scaduto';
+    }
+    if (this.giorniAScadenza() == 0) {
+      return 'Scade oggi';
+    }
+    if (this.giorniAScadenza() < 7) {
+      return 'Scade fra ' + this.giorniAScadenza() + ' giorni.';
+    }
+    return this.dataScadenza.toLocaleDateString();
+  }
+
+  getScadenzaShortLabel(): string {
+    if (this.giorniAScadenza() < 0) {
+      return 'S';
+    }
+    if (this.giorniAScadenza() == 0) {
+      return 'O';
+    }
+    if (this.giorniAScadenza() < 7) {
+      return this.giorniAScadenza().toString();
+    }
+    return 'V';
+  }
+
+  giorniAScadenza(): number {
+    let diffInTime = this.dataScadenza.getTime() - new Date().getTime();
+    return Math.round(diffInTime / (1000 * 3600 * 24));
+  }
+
+  daAcquistare(): boolean {
+    return this.quantita < MINSTOCK && this.nome != '';
   }
 }
